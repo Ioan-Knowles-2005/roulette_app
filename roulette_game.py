b7a4cda909ff6f77306eb12d5a9de3359540a856
Ioan-Knowles-2005 <s2494818@ed.ac.uk>
2025-02-16 21:03:46 +0000

    Update roulette_game.py

--- roulette_game.py
+++ roulette_game.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import random
 from PIL import Image
-from scipy.stats import norm  # (Not used in roulette but kept from your original code)
 import matplotlib.pyplot as plt
 
 # --- Helper Functions ---
